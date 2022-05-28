cafad3513de983aeab09b6bfaff90d55dabcde5f
Hurtle666 <84672409+Hurtle666@users.noreply.github.com>
2022-05-27 21:15:23 -0400

    Add files via upload
    play along with it guys,its a project am working on
    hoping i can finish it (lol)
    use it for graphical prank

---
+++ crypto.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+none="\033[0m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+red="\033[1;31m"
+#This project is just for fun @Find_X
+echo -e $red
+echo  -e $red"======================================"$none$yellow"][[[][][]"$none$red"=============================="$none
+echo -e "\033[1;33m"
+  figlet -w 5 -t "crypto_flash"
+          echo -e $green"github" $none$yellow"@Hurtle66"$none
+echo -e $red"======================================"$none$yellow"[][][][]]"$none$red"=============================="$none
+echo -e $green"Choose the coin you wanna flash with"
+echo -e $red"["$none$yellow"B"$none$red"]"$none $yellow"Bitcoin"$none
+echo -e $red"["$none$yellow"E"$none$red"]"$none $yellow"Etherium"$none
+echo -e $red"["$none$yellow"S"$none$red"]"$none $yellow"Solana"$none
+echo -e $red"["$none$yellow"A"$none$red"]"$none $yellow"ApeCoin"$none
+echo -e $red"["$none$yellow"M"$none$red"]"$none $yellow"Munero"$none
+echo -e $red"["$none$yellow"U"$none$red"]"$none $yellow"Usdt"$none
+echo -e $red"(\_/)"
+echo -e $red"(o_o)"$none$green"Am your Doom!!!!!!"$none
+echo -e $red"/>[]/>"$none
+echo -e $red"[]_[]_"
+echo  "#####################//////#############################"
+echo -e $red"#########"$green"@Hurtle666"$none$red"##########///////##################"
+echo "################///////##################"
+echo -e $red"###############"$none$green"crypto_Flash"$none$red"###########"
+echo -e $red"###########///////###################"
+echo -e $red"########////////##################"
+echo -e $red"######///////##################"
+echo -e $red"####//////////###########"
+echo -e $red"##//////###########"
+echo -e $red"##////////######"
+echo -e $red"#/////#######"
+echo -e $red"#////#####"
+echo -e $red"#////###"
+echo -e $red"#//###"
+echo -e $red"#//#"
+echo -e $red"#/#"
+echo -e $red"#/"
+echo -e $red"#"
+
+
+echo -e $green"Choose your choice"$none
+read
+echo -e $green"input victim wallet address"$none
+read
+echo -e $green"Enter Digit"$none
+read
+echo -e $yellow"input your password"$none
+read
+
+echo -e $red"["$none$yellow"S"$none$red"]"$none $yellow"SEND"$none
+read
+echo -e "\033[1;31m"
+figlet -t -c "Crypto_Wallet_Flash"
+echo -e $yellow"Am not responsible for any criminal activites you commit with this tool"$none
+echo -e $red"Use tool for prank only"$none
+echo -e $green"This is one in a million tool which flashes  crypto_wallet address with coin,But imediately after 3hour's the value returns back to Zero"$none
+echo -e $red"Wish you the best using it"$none
+echo -e $yellow"follow on twitter"$none$green"@GeoHurtle"$none
+echo -e $yellow"follow on github"$none$green"@Hurtle666"$none
+echo -e $yellow"Turn on notification get it when its done!!!!!!"$none
